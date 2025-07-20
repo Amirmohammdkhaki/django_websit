73af4e998b17c601c49dfc6c1d708e5c8f09945d
amir <mohammdkhaki2022@gmail.com>
2025-07-21 02:18:40 +0330

    websit3

--- books/views.py
+++ books/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Books
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy ,reverse
 
 class BookListView(generic.ListView):
     model = Books
@@ -18,3 +18,26 @@ class BookCreateView(generic.CreateView):
     template_name = 'books/book_create.html'
     fields = ['title', 'author', 'description', 'price']
     success_url = reverse_lazy('book_list')  # بعد از ایجاد، به لیست هدایت شود
+
+
+
+class BookUpdateView(generic.UpdateView):
+    model = Books
+    template_name = 'books/book_update.html'
+    fields = ['title', 'author', 'description', 'price']
+    
+    def get_success_url(self):
+        return reverse('book_detail', kwargs={'pk': self.object.id})
+    
+
+class BookDeleteView(generic.DeleteView):
+    model = Books
+    template_name = 'books/book_delete.html'
+    success_url = reverse_lazy('book_list')
+    context_object_name = 'book'
+
+    
+    
+    
+
+
