57c114e3fed5c9df76b3e2ab267ddd7c266b96ec
amir <mohammdkhaki2022@gmail.com>
2025-07-21 00:57:41 +0330

    edit5

--- books/views.py
+++ books/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Books
+from django.urls import reverse_lazy
 
 class BookListView(generic.ListView):
     model = Books
@@ -11,3 +12,9 @@ class BookDetailView(generic.DetailView):
     model = Books
     template_name = 'books/book_detail.html'
     context_object_name = 'book'
+
+class BookCreateView(generic.CreateView):
+    model = Books
+    template_name = 'books/book_create.html'
+    fields = ['title', 'author', 'description', 'price']
+    success_url = reverse_lazy('book_list')  # بعد از ایجاد، به لیست هدایت شود
